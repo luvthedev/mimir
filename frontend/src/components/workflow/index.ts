e07f4722c2f201ac7b0d6afb046fe9ad9880ff0a
Luv Jain <luv.jain01@gmail.com>
2026-02-24 10:25:24 -0500

    Implement: Build interactive drag-and-drop canvas for workflow construction

---
+++ frontend/src/components/workflow/index.ts
@@ -0,0 +1,5 @@
+export { WorkflowEditor } from './WorkflowEditor';
+export { Canvas } from './Canvas';
+export { NodePalette } from './NodePalette';
+export { WorkflowNode } from './WorkflowNode';
+export { ConnectionLine, PendingConnectionLine } from './ConnectionLine';
